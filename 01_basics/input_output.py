55b769c1146af64d3b05832d2f6f000d3be717f5
snippetLab <smit.ppatel11@gmail.com>
2025-07-16 06:21:57 +0000

    Input-Output Completed

---
+++ 01_basics/input_output.py
@@ -0,0 +1,55 @@
+# Take User Input :
+# print("Enter employee name : ");
+# empName = input();
+# print(f"Welcom {empName}");
+
+# Using prompt :
+# empName = input("Enter employee name : ");
+# print(f"Welcome {empName}");
+
+# Multiple Inputs :
+# empName = input("Enter employee name : ");
+# print(f"Hello {empName}");
+
+# empAge = input("Enter employee Age : ");
+# empEdu = input("Enter employee eduction : ");
+# empExp = input("Enter employee experience : ");
+# print(f"Employee's Age {empAge}, Employee's Education {empEdu}, Employee's Experience {empExp} Years");
+
+# Number Input :
+# import math; 
+# numOne = input("Enter number : ");
+# numSqr = math.sqrt(float(numOne)); # Square root of number
+# print(f"Square root {numSqr}");
+
+# Validate Input :
+# numTest = True;
+# while numTest == True :
+#     numOne = input("Enter number : ")
+#     try :
+#         numOne = float(numOne)
+#         numTest = False
+#     except :
+#         print("Wrong Input");
+# print("Thank You!");
+
+
+# Output :
+empName = "Joe Root";
+print(empName);
+
+empNameOne = "Joe";
+empNameTwo = "Root";
+print(empNameOne, empNameTwo);
+print(empNameOne + " " + empNameTwo);
+
+empOldId = 1;
+empNewId = 8;
+print(empOldId + empNewId);
+
+empName = "Joe Root";
+empId = 1;
+# print(empId + ":" + empName); # Error
+print(empId, empName);
+
+
