62a018dd6c831f095519a7a29299570e122ec0f2
snippetLab <smit.ppatel11@gmail.com>
2025-07-16 04:25:12 +0000

    If-else Completed

---
+++ 02_conditions_loops/if_else.py
@@ -0,0 +1,79 @@
+# numOne = 500;
+# numTwo = 300;
+
+# if numOne > numTwo :
+#     print("Number One");
+
+# numOne = 38;
+# numTwo = 38;
+
+# if numOne > numTwo :
+#     print("Number One");
+
+# elif numOne == numTwo :
+#     print("Number One == Number Two");
+
+# numOne = 8;
+# numTwo = 11;
+
+# if numOne < numTwo:
+#     print("Number Two");
+
+# elif numOne == numTwo:
+#     print("Number One == Number Two");
+
+# else :
+#     print("Number One");
+
+# numOne = 3;
+# numTwo = 1;
+
+# if numOne < numTwo :
+#     print("Number One");
+
+# else :
+#     print("Number Two");
+
+# numOne = 13;
+# numTwo = 5;
+
+# print("Number One") if numOne > numTwo else print("Number Two");
+
+# numOne = 19;
+# numTwo = 19;
+# print("Number One") if numOne > numTwo else print("Number One == Number Two") if numOne == numTwo else print("Number Two");
+
+# numOne = 89;
+# numTwo = 30;
+# numThree = 95;
+
+# if numOne > numTwo and numThree > numOne :
+#     print("All Conditions Valid");
+
+# numOne = 89;
+# numTwo = 30;
+# numThree = 95;
+
+# if numOne > numTwo or numThree > numOne :
+#     print("One Condition Valid");
+
+# numOne = 30;
+# numTwo = 89;
+# if not numOne > numTwo :
+#     print("Number One < Number Two");
+
+# num = 84;
+# if num > 10 :
+#     print("Above Ten")
+
+#     if num > 40 :
+#         print("Above Fotry");
+
+# else :
+#     print("Not Above Forty");
+
+numOne = 3;
+numTwo = 1;
+
+if numOne > numTwo :
+    pass;
