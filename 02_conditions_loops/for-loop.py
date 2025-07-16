2cdb07788dbc336bfd54e78431cfbb0cb5b22bcd
snippetLab <smit.ppatel11@gmail.com>
2025-07-16 05:40:05 +0000

    For Loop Completed

---
+++ 02_conditions_loops/for-loop.py
@@ -0,0 +1,51 @@
+# empName = ["Mark", "Elon", "Root", "Jack","Ken"];
+# for emp in empName :
+#     print(emp);
+
+# for word in "Snippet" :
+#     print(word);
+
+# empName = ["Mark", "Elon", "Root", "Jack","Ken"];
+# for emp  in empName :
+#     print(emp)
+#     if emp == "Jack" :
+#         break
+
+# empName = ["Mark", "Elon", "Root", "Jack","Ken"];
+# for emp in empName :
+#     if emp == "Root" : 
+#         continue
+#     print(emp)
+
+# range() function :
+# for num in range(6) :
+#     print(num);
+
+# for num in range(3, 6) :
+#     print(num);
+
+# for num in range(1, 30, 3) :
+#     print(num);
+
+# for num in range(6) :
+#     print(num);
+# else :
+#     print("Loop executed");
+
+# for num in range(6) :
+#     if num == 3 :
+#         break
+#     print(num);
+# else :
+#     print("Loop executed");
+
+# empName = ["Mark", "Elon", "Root", "Jack", "Ken"];
+# empNameOne = ["Zukerberg", "Musk", "Joe", "Brayn", "Willy"];
+
+# for out in empName :
+#     for ine in empNameOne :
+#         print(out, ine);
+
+
+for num in [1, 2, 3] :
+    pass
