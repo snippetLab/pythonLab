5a6b87e37f55d60ae9c61643c9277d8357062731
snippetLab <smit.ppatel11@gmail.com>
2025-07-18 02:30:38 +0000

    Arrays Completed

---
+++ 04_data_stracture/arrays.py
@@ -0,0 +1,20 @@
+empName = ["Mark", "Elon", "Root", "Jack"];
+# print(empName);
+# print(empName[1]); # Access Array Element
+# print(len(empName));
+
+# Looping Array Elements :
+# empName = ["Mark", "Elon", "Root", "Jack"];
+# for emp in empName :
+#     print(emp);
+
+# Adding Array Elements :
+# empName = ["Mark", "Elon", "Root", "Jack"];
+# empName.append("Magnus");
+# print(empName);
+
+# Removing Array Elements :
+empName = ["Mark", "Elon", "Root", "Jack"];
+# empName.pop(1);
+empName.remove("Mark");
+print(empName);
