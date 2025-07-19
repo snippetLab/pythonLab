d1d9431ccc64429e72f5fe25fd8ad97b27ff9842
snippetLab <smit.ppatel11@gmail.com>
2025-07-19 11:36:15 +0000

    F-string Completed

---
+++ str-format.py
@@ -0,0 +1,27 @@
+# print(f"Employee Name : Joe Root"); # F-string
+
+empOne = "Brayn";
+empMoney = 10000
+# print(f"Employee One Name : {empOne}"); # Placeholder : {empOne}
+# print(f"Employee One PayCheck : {empMoney:.2f}"); # Modifier : {.2f}
+# print(f"Employee One Paycheck : {10000:.2f}"); # Without Variable
+
+# Operations in F-String :
+# print(f"Addition : {3 + 8}");
+
+# numOne  = 11;
+# numTwo = 28;
+# print(f"Multiplication : {numOne * numTwo}");
+
+# empExpe = 13;
+# print(f"Employee Status : {"Senior" if empExpe > 13 else "Junior"}");
+
+# Function Execution in F-String :
+favMovie = "Ironman";
+# print(f"Favourite Movie : {favMovie.upper()}");
+
+# def myFunc(num) :
+#     return num * 3.88;
+# print(f"New Number : {myFunc(11)}");
+
+
