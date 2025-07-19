b42e56a3b86be2e0c0b99dd6d04d1aa5630ff7ea
snippetLab <smit.ppatel11@gmail.com>
2025-07-19 04:29:02 +0000

    Math Completed

---
+++ 07_module/mod_math.py
@@ -0,0 +1,22 @@
+# Built-in Math Functions :
+# minNum = min(1, 2, 3);
+# maxNum = max(1, 2, 3);
+# print(minNum); # Finds Minimum Number
+# print(maxNum); # Finds Maximum Number
+# print(abs(-8.33)); 
+# pNum = pow(3,3);
+# print(pNum);
+
+# Math Module :
+import math;
+
+xNum = math.sqrt(81);
+print(xNum);
+
+xCeil = math.ceil(2.5);
+xFloor = math.floor(2.5);
+print(xCeil);
+print(xFloor);
+
+xPie = math.pi;
+print(xPie);
