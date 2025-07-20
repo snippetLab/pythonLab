1a30dee1df730e26ca42ca66600242deedf5ecd4
snippetLab <smit.ppatel11@gmail.com>
2025-07-20 03:01:23 +0000

    Comments Completed

---
+++ 01_basics/comments.py
@@ -0,0 +1,9 @@
+# Comment
+print("Comment"); # Explanation
+# print("Comment");
+
+# Multiline Comments :
+# A snippet is a small piece or fragment of something, often used to 
+# refer to a short excerpt or portion of text, code, or other media.
+
+"""A snippet is a small piece or fragment of something, often used to refer to a short excerpt or portion of text, code, or other media."""
