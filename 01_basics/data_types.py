81cdbce14c8ad9229649f7792d118c911166385a
snippetLab <smit.ppatel11@gmail.com>
2025-07-20 03:12:36 +0000

    Variables Completed

---
+++ 01_basics/data_types.py
@@ -0,0 +1,46 @@
+# Built-in Data Types :
+# print("Text Type : str");
+# print("Numeric Type : int, float, complex");
+# print("Sequence Type : list, tuple, range");
+# print("Mapping Type : dict");
+# print("Set Type : set, frozenset");
+# print("Boolean Type : bool");
+# print("Binary Type : bytes, bytearray, memoryview");
+# print("None Type : NoneType");
+
+# Know Data Types :
+numOne = 11;
+# print(type(numOne));
+
+# set Data Type
+x = "Snippet";
+x = 11;
+x = 11.3;
+x = 11j;
+x = ["Mark", "Elon", "Root", "Jack"];
+x = ("Mark", "Elon", "Root", "Jack");
+x = range(3);
+x = {"Name" : "Joe Root", "Age" : 35};
+x = {"Mark", "Elon", "Root", "Jack"};
+x = frozenset({"Mark", "Elon", "Root", "Jack"});
+x = True;
+x = b"Snippet";
+x = bytearray(3);
+x = memoryview(bytes(3));
+x = None;
+
+# Set Specific Data Type :
+x = str("Snippet");
+x = int(11);
+x = float(11.3);
+x = complex(11j);
+x = list(("Mark", "Elon", "Root", "Jack"));
+x = tuple(("Mark", "Elon", "Root", "Jack"));
+x = range(3);
+x = dict(Name = "Joe Root", Age = 35);
+x = set(("Mark", "Elon", "Root", "Jack"));
+x = frozenset(("Mark", "Elon", "Root", "Jack"));
+x = bool(3);
+x = bytes(3);
+x = bytearray(3);
+x = memoryview(bytes(5));
