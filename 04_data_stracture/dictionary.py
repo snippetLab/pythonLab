63f2385c4bc955d8ebce54eab78bf0d46077ca7f
snippetLab <smit.ppatel11@gmail.com>
2025-07-21 04:14:03 +0000

    Dicionary Completed

---
+++ 04_data_stracture/dictionary.py
@@ -0,0 +1,51 @@
+empOne = {
+    "Name" : "Joe Root",
+    "Age" : 35,
+    "Country" : "England",
+    "Experience" : "8 Years"
+};
+
+empOne = {
+    "Name" : "Joe Root",
+    "Age" : 35,
+    "Country" : "England",
+    "Experience" : "8 Years",
+    "Country" : "Russia"
+}; # Not Allows Duplicate
+
+# Dictionary Length :
+empOne = {
+    "Name" : "Joe Root",
+    "Age" : 35,
+    "Country" : "England",
+    "Experience" : "8 Years"
+};
+# print(len(empOne));
+
+# Dictionary Items : Data Types :
+empOne = {
+    "Name" : "Joe Root",
+    "Age" : 35,
+    "Country" : "England",
+    "Experience" : "8 Years",
+    "Hired" : True,
+    "Skills" : ["JavaScript", "SQL", "Network Administartion"]
+};
+
+# Dicitonary Type :
+empOne = {
+    "Name" : "Joe Root",
+    "Age" : 35,
+    "Country" : "England",
+    "Experience" : "8 Years"
+};
+print(type(empOne));
+
+# The dict() Constructor :
+empOne = dict(
+    Name = "Joe Root",
+    Age = 35,
+    Country = "England",
+    Experience = "8 Years"
+);
+print(empOne);
