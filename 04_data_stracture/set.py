1d7d3e43d3b2459ecad94a32f8d1b248b66bc110
snippetLab <smit.ppatel11@gmail.com>
2025-07-21 04:07:37 +0000

    Changes

---
+++ 04_data_stracture/set.py
@@ -0,0 +1,21 @@
+empName = {"Mark", "Elon", "Root", "Jack"};
+# empName = {"Mark", "Elon", "Root", "Jack", "Root", "Mark"}; # Not Allows Duplicates
+
+# Set Length :
+empName = {"Mark", "Elon", "Root", "Jack"};
+# print(len(empName));
+
+# Set Items - Data Types :
+empName = {"Mark", "Elon", "Root", "Jack"}; # str
+empId = {1, 2, 3, 4}; # int
+empAvailable = {True, False, True}; # bool
+empAbout = {"Root", 1, True, 11.3, "Mark", False}; # Mix 
+
+# Set Type :
+empName = {"Mark", "Elon", "Root", "Jack"};
+# print(type(empName));
+
+# The Set() Constructor :
+empName = set(("Mark", "Elon", "Root", "Jack"));
+# print(empName);
+
--- 04_data_stracture/tuple.py
+++ 04_data_stracture/tuple.py
@@ -1,21 +1,21 @@
 empName = ("Mark", "Elon", "Root", "Jack");
 empName = ("Mark", "Elon", "Root", "Jack", "Root", "Mark"); # Allows Duplicates
 
-# List Length :
+# Tuple Length :
 empName = ("Mark", "Elon", "Root", "Jack");
 # print(len(empName));
 
-# List Items - Data Types :
+# Tuple Items - Data Types :
 empName = ("Mark", "Elon", "Root", "Jack"); # str
 empId = (1, 2, 3, 4); # int
 empAvailable = (True, False, True); # bool
 empAbout = ("Root", 1, True, 11.3, "Mark", False); # Mix 
 
-# List Type :
+# Tuple Type :
 empName = ("Mark", "Elon", "Root", "Jack");
 # print(type(empName));
 
-# The list() Constructor :
-empName = list(("Mark", "Elon", "Root", "Jack"));
+# The Tuple() Constructor :
+empName = tuple(("Mark", "Elon", "Root", "Jack"));
 # print(empName);
 
