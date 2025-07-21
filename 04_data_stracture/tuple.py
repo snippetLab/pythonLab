0f2e10d940e936cceaa3660f6d1247629c33816b
snippetLab <smit.ppatel11@gmail.com>
2025-07-21 04:05:47 +0000

    Tuple Completed

---
+++ 04_data_stracture/tuple.py
@@ -0,0 +1,21 @@
+empName = ("Mark", "Elon", "Root", "Jack");
+empName = ("Mark", "Elon", "Root", "Jack", "Root", "Mark"); # Allows Duplicates
+
+# List Length :
+empName = ("Mark", "Elon", "Root", "Jack");
+# print(len(empName));
+
+# List Items - Data Types :
+empName = ("Mark", "Elon", "Root", "Jack"); # str
+empId = (1, 2, 3, 4); # int
+empAvailable = (True, False, True); # bool
+empAbout = ("Root", 1, True, 11.3, "Mark", False); # Mix 
+
+# List Type :
+empName = ("Mark", "Elon", "Root", "Jack");
+# print(type(empName));
+
+# The list() Constructor :
+empName = list(("Mark", "Elon", "Root", "Jack"));
+# print(empName);
+
